2914789af5581f6b8be6425a9d3c54e4b7fd80ba
arch <arch@local>
2023-03-18 17:19:13 +0100

    typo

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-LOG_LEVEL=DEBUG VOLUMES_CONFIG_PATH=./example/storage/config/volumes.yaml python3 ou deploy-samba-server.py
+LOG_LEVEL=DEBUG VOLUMES_CONFIG_PATH=./example/storage/config/volumes.yaml python3 -u deploy-samba-server.py
